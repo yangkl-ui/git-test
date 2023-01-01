80265655411dcbdf8dce566dd32bbe63ac6cbb2f
yangkl-ui <2582610878@qq.com>
2023-01-01 17:28:39 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @version 1.0
+ * @Author KL
+ * @Date 2023/1/1 17:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+}
