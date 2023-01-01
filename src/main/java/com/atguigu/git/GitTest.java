f4409269f66a9ab8ff4e98ba46f4ff09127f4e7d
yangkl-ui <75553690+yangkl-ui@users.noreply.github.com>
2023-01-01 18:05:16 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("git test2 master");
         System.out.println("git test2 hot-fix");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
